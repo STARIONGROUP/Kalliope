30f7e7e398e7c16425c33c96cf2e335aba175724
sam.gerene <s.gerene@rheagroup.com>
2022-09-19 14:34:51 +0200

    [Add] Kalliope.OO library

---
+++ Kalliope.OO.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+
+
+namespace Kalliope.OO.Tests
+{
+    using NUnit.Framework;
+
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
